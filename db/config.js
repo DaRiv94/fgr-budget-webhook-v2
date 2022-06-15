811a6d0b408629b947f49f80accd816d920a8799
Frankie Riviera <frankgriviera@outlook.com>
2022-06-15 08:30:19 -0400

    added ssl require in seqeuilze cli config

--- db/config.js
+++ db/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 let configObject={}
 if(process.env.DATABASE_URL){
     connection_string_to_parse = process.env.DATABASE_URL
-    var regex = new RegExp('[:/@]')
+    var regex = new RegExp('[:/@?]')
     const connection_string_array_to_parse = connection_string_to_parse.split(regex);
     let connectionstring_array = connection_string_array_to_parse.slice(3)
 
@@ -12,6 +12,12 @@ if(process.env.DATABASE_URL){
     configObject['DB_HOST'] = connectionstring_array[2]
     configObject['DB_PORT'] = connectionstring_array[3]
     configObject['DB'] = connectionstring_array[4]
+
+    console.log("configObject.DB_USERNAME " + configObject.DB_USERNAME)
+console.log("configObject.DB_PASSWORD " + configObject.DB_PASSWORD)
+console.log("configObject.DB " + configObject.DB)
+console.log("configObject.DB_HOST " + configObject.DB_HOST)
+console.log("configObject.DB_PORT " + configObject.DB_PORT)
 }
 
 
@@ -30,6 +36,19 @@ module.exports = {
         database: configObject.DB,
         host: configObject.DB_HOST,
         port: configObject.DB_PORT,
-        dialect: 'postgres'
+        dialect: 'postgres',
+        ssl:true,
+        dialectOptions:{
+           ssl:{
+              require:true
+           }
+        }
     }
   };
+
+
+//   "ssl":true,
+//   "dialectOptions":{
+//      "ssl":{
+//         "require":true
+//      }
